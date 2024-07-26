c650e28c3e3dc14dc74102659f1158caab55c130
Tuyen Tran <Tuyen.TranThiBich@nashtechglobal.com>
2024-07-26 15:03:50 +0700

    update ssh file

--- ssh.tf
+++ ssh.tf
@@ -3,15 +3,6 @@ resource "random_pet" "ssh_key_name" {
   separator = ""
 }
 
-resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
-
-  response_export_values = ["publicKey", "privateKey"]
-}
-
 resource "azapi_resource" "ssh_public_key" {
   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   name      = random_pet.ssh_key_name.id
@@ -19,6 +10,15 @@ resource "azapi_resource" "ssh_public_key" {
   parent_id = azurerm_resource_group.rg.id
 
   tags = module.label.tags
+}
+
+resource "azapi_resource_action" "ssh_public_key_gen" {
+  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
+  resource_id = azapi_resource.ssh_public_key.id
+  action      = "generateKeyPair"
+  method      = "POST"
+
+  response_export_values = ["publicKey", "privateKey"]
 }
 
 output "key_data" {
